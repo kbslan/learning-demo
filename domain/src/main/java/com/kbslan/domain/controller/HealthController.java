d08d30d3cd4e535270121fb7a1c7be532c79d11e
chao.lan <chao.lan@dmall.com>
2023-08-22 18:00:44 +0800

    feat(master): move file

--- domain/src/main/java/com/kbslan/domain/controller/HealthController.java
+++ domain/src/main/java/com/kbslan/domain/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.kbslan.sentinel;
+package com.kbslan.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
